2ae9ddd06648ec590a4c16df65b86711ee4cf81e
eengsf <shofiyulfuad797@gmail.com>
2025-02-22 12:10:48 +0700

    update status navbar

--- components/navbar/UserIn.tsx
+++ components/navbar/UserIn.tsx
@@ -9,10 +9,10 @@ function UserIn() {
       <div className="flex gap-1 items-center relative md:cursor-default cursor-pointer">
         <div className="relative group">
           <Image
-            src={session.user?.image ?? '/user.svg'}
+            src={session.user?.image ? session.user?.image : '/user.png'}
             alt="Profile"
-            width={7}
-            height={7}
+            width={500}
+            height={500}
             className="w-7 h-7 rounded-full"
           />
 
@@ -34,7 +34,35 @@ function UserIn() {
       </button>
     </div>
   ) : (
-    <span className='text-sm text-light'>loading...</span>
+    // <span className='text-sm text-light'>loading...</span>
+    <div className="flex gap-5 items-center">
+      <div className="flex gap-1 items-center relative md:cursor-default cursor-pointer">
+        <div className="relative group">
+          <Image
+            src={'/user.png'}
+            alt="Profile"
+            width={500}
+            height={500}
+            className="w-10 h-10 rounded-full border border-white p-1"
+          />
+
+          <div className="md:hidden block absolute flex-col p-2 rounded-md opacity-0 group-hover:opacity-100 transition-all duration-300 bg-sunMedium dark:bg-moonThin text-dark dark:text-light right-0 mt-2 shadow-lg">
+            <p className="text-xs">Shofiyul Fuad</p>
+            <p className="text-xs">example@gmail.com</p>
+          </div>
+        </div>
+        <div className="md:flex hidden flex-col">
+          <p className="text-xs">Shofiyul Fuad</p>
+          <p className="text-xs">example@gmail.com</p>
+        </div>
+      </div>
+      <button
+        
+        className="rounded-full transition-all duration-300 hover:bg-sunMedium dark:hover:bg-moonThin p-2"
+      >
+        <Logout />
+      </button>
+    </div>
   );
 }
 
