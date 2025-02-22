5f556251eaa58d5449931baf7b9fa474c7bab002
eengsf <shofiyulfuad797@gmail.com>
2025-02-22 08:47:19 +0700

    fix

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/"], // Middleware hanya berlaku di /dashboard
+  matcher: ["/dashboard", "/"], // Middleware hanya berlaku di /dashboard
 };
